eb95fbd49c32babba8f52875b211c6918e904d0f
Santhosh1043k <159894976+Santhosh1043k@users.noreply.github.com>
2024-02-13 16:47:05 +0530

    Add files via upload

---
+++ forLoop/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        
+        for (int n = 15; n <= 19; n++){
+            System.out.println(n);
+        }
+    }
+}
